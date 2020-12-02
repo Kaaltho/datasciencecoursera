ffac5b64691f582fc739bd1edf0306abf63595e6
Kaaltho <hoskweg@gmail.com>
2020-12-02 13:50:15 -0600

    Final Version

--- Getting and Cleaning Data/Week four/Graded Assignment.R
+++ Getting and Cleaning Data/Week four/Graded Assignment.R
@@ -35,5 +35,44 @@ dataActivity <- rbind(dataActtrain, dataActtest)
 dataSubject <- rbind(dataSubjtrain, dataSubjtest)
 dataFeatures <- rbind(dataFeattrain, dataFeattest)
 
+names(dataSubject) <- c("subject")
+names(dataActivity) <- c("activity")
+dataFeaturesNames <- read.table(file.path(files,"features.txt"), head=FALSE)
+names(dataFeatures) <- dataFeaturesNames$V2
+
+dataCombine <- cbind(dataSubject, dataActivity)
+datos <- cbind(dataFeatures, dataCombine)
+
+#Extracting measurements of mean and standard deviation
+
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+datos1<-subset(datos,select=selectedNames)
+
+#Descriptive names
+
+activityLabels <- read.table(file.path(files, "activity_labels.txt"),header = FALSE)
+datos1$activity<-factor(datos1$activity,labels=activityLabels[,2])
+
+#Setting labels with descriptive variable names
+names(datos1)<-gsub("^t", "time", names(datos1))
+names(datos1)<-gsub("^f", "frequency", names(datos1))
+names(datos1)<-gsub("Acc", "Accelerometer", names(datos1))
+names(datos1)<-gsub("Gyro", "Gyroscope", names(datos1))
+names(datos1)<-gsub("Mag", "Magnitude", names(datos1))
+names(datos1)<-gsub("BodyBody", "Body", names(datos1))
+
+#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+install.packages("plyr")
+library(plyr)
+datos2<-aggregate(. ~subject + activity, datos1, mean)
+datos2<-datos2[order(datos2$subject,datos2$activity),]
+write.table(datos2, file = "independenttidydata.txt",row.name=FALSE)
+
+#Generating a Coodebook
+install.packages("codebook")
+library(codebook)
+new_codebook_rmd()
 
 
