6bb1f8ef76f547496c7d28173ce3b5a5348ecf3a
Kaaltho <hoskweg@gmail.com>
2020-12-02 10:52:18 -0600

    Adquiring data

---
+++ Getting and Cleaning Data/Week four/Graded Assignment.R
@@ -0,0 +1,16 @@
+#Peer-graded Assignment: Getting and Cleaning Data Course Project
+
+#Getting the data
+setwd("F:/R codes/Projects/datasciencecoursera/Getting and Cleaning Data")
+if(!file.exists("Week four")){
+  dir.create("Week four")
+  setwd("F:/R codes/Projects/datasciencecoursera/Getting and Cleaning Data/Week four")
+}  
+# File download
+ulrfile <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+dirfile <- "F:/R codes/Projects/datasciencecoursera/Getting and Cleaning Data/Week four/Dataset.zip"
+  
+download.file(ulrfile, destfile =dirfile, method = "curl")
+
+
+
